d514bbacc1ffe1ed183d50f7042ec4c73b1d86d6
cmndoza <cmndoza@yahoo.com.mx>
2024-11-23 14:47:22 -0600

    Update 10_Ejercicio_6.py

--- 0000_POLI/10_Ejercicio_6.py
+++ 0000_POLI/10_Ejercicio_6.py
@@ -7,7 +7,6 @@ if numero % 2 == 0:
 else:
     print(f"{numero} es un número impar.")
 
-
 # for numero in range(1, 11):  # Recorre los números del 1 al 10
 #     if numero % 2 == 0:
 #         print(f"{numero} es par.")
