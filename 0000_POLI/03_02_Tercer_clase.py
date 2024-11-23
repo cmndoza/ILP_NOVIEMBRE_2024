8dc36e36285503a618ecf3345218907d77579634
cmndoza <cmndoza@yahoo.com.mx>
2024-11-23 11:50:42 -0600

    adición de ejecicio

---
+++ 0000_POLI/03_02_Tercer_clase.py
@@ -0,0 +1,15 @@
+# Número que deseas verificar
+numero = 7
+
+# Identificar si es par o impar
+if numero % 2 == 0:
+    print(f"{numero} es un número par.")
+else:
+    print(f"{numero} es un número impar.")
+
+
+# for numero in range(1, 11):  # Recorre los números del 1 al 10
+#     if numero % 2 == 0:
+#         print(f"{numero} es par.")
+#     else:
+#         print(f"{numero} es impar.")
---
+++ 0000_POLI/10_Ejercicio_6.py
@@ -0,0 +1,15 @@
+# Número que deseas verificar
+numero = int(input("Introduzca el número a evaluar:"))
+
+# Identificar si es par o impar
+if numero % 2 == 0:
+    print(f"{numero} es un número par.")
+else:
+    print(f"{numero} es un número impar.")
+
+
+# for numero in range(1, 11):  # Recorre los números del 1 al 10
+#     if numero % 2 == 0:
+#         print(f"{numero} es par.")
+#     else:
+#         print(f"{numero} es impar.")
