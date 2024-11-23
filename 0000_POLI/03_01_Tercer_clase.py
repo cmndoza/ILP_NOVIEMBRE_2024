c08ae66ea563c751048458253d87f6bde0f8bb2b
cmndoza <cmndoza@yahoo.com.mx>
2024-11-23 11:39:28 -0600

    Update 03_01_Tercer_clase.py

--- 0000_POLI/03_01_Tercer_clase.py
+++ 0000_POLI/03_01_Tercer_clase.py
@@ -1,4 +1,8 @@
-edad = int(input("Escribe tu edad: "))
+from decimal import Decimal, ROUND_HALF_UP
+
+edad = float(input("Escribe tu edad: "))
+edad = edad.quatize(Decimal('0.0'), rounding=ROUND_HALF_UP)
+#edad = round(edad, 1)
 
 #RANGAO VÁLIDO DE EDAD ENTRE 18 Y 120 AÑOS
 if edad < 0 or edad > 120:
