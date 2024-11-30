3f982c21f24152194239bba5eeb17721cccf53ae
cmndoza <cmndoza@yahoo.com.mx>
2024-11-30 12:39:50 -0600

    Update 10_Ejercicio_9.py

--- 0000_POLI/10_Ejercicio_9.py
+++ 0000_POLI/10_Ejercicio_9.py
@@ -1,6 +1,7 @@
 import random
 import string
 import os
+from datetime import datetime, timedelta
 #para presentar un menú
 def Mostrar_Menu():
     print("******** MENÚ *********")
@@ -15,7 +16,23 @@ def Lista_Transportes():
     print(Lista_pasajes, Lista_calificaciones)
 
 def Calcular_Nomina ():
-    print("Nómina")
+    fecha_inicio_str = "01/05/2023"
+    fecha_fin_str = "31/05/2023"
+    fecha_inicio = datetime.strptime(fecha_inicio_str, "%d/%m/%Y")
+    fecha_fin = datetime.strptime(fecha_fin_str, "%d/%m/%Y")
+    #creación del valor correspondiente al día cero, formato delta, para poder operarse
+    dia_cero = timedelta(days=1)
+    # Calcular la diferencia en días
+    extension_dias = (fecha_fin - fecha_inicio) + dia_cero
+    # Convertir a entero
+    extension_dias = int(extension_dias.days)
+    pago = int(250)
+    pago_mensual = pago * extension_dias
+    #Para darle formato de moneda con dos decimales
+    pago_formateado = f"${pago_mensual:.2f}"
+
+    print("Con base en los días que tiene el mes trabajado,",extension_dias, " días, el pago correspondiente de nómina es:", pago_formateado)
+    #print("Nómina")
 
 def Generar_contraseña(numero_caracteres):
     if (numero_caracteres <= 5):
